4d53c3fba9bf522e903672b011734ccbc171ea93
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-05 18:41:46 -0600

    added navbar,buttons, divs, columns and rows

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,13 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Generate random password
+function Generate() {
+  //set password length/complexity
+  let complexity = document.getElementById("slider").nodeValue;
 
-// generate password
-function generatePassword();{
- // prompt the user for password length
-//  cheeck to see if user entered length 
-// 4 confirms
-// special characters 
-// numbers 
-// lowercase
-// uppercase
-// generate password, based on user specifications.
-//  user specifications come from confirms 
-// save them in variables
-// 
- for(var i= 0; i < 10; i++){
-   
- }
+  let values =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz `~!@#$%^&*()-_=+[]{}| ...";
 
+  let password ="";
 
-  return "something";
-}
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // create for loop to choose password characters
+  }
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
