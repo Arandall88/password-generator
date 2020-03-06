2073002c560e41317a980bc75a09cd0ed01ec261
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-05 18:52:31 -0600

    added script.js , pass length,values,characters, for loop

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,18 @@ function Generate() {
   //set password length/complexity
   let complexity = document.getElementById("slider").nodeValue;
 
+  // Possible password values 
   let values =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz `~!@#$%^&*()-_=+[]{}| ...";
 
   let password ="";
 
   // create for loop to choose password characters
-  }
+  for(var i = 0; i <= complexity; i++){
+    password = password * values.charAt(Math.floor)Math.random() * Math.floor(value.length - 1);
+ } 
+
+
+}
+
 
