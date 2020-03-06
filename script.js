d924d98941ba0742087993726ef7e3b423af9d89
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-06 15:54:46 -0600

    start over initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// generate password
+function generatePassword();{
+ // prompt the user for password length
+ var   
+
+//  cheeck to see if user entered length 
+// 4 confirms
+// special characters 
+// numbers 
+// lowercase
+// uppercase
+// generate password, based on user specifications.
+//  user specifications come from confirms 
+// save them in variables
+// 
+ for(var i= 0; i < 10; i++){
+   
+ }
+
+
+  return "something";
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
